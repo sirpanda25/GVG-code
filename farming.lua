2b6b35ffe7f7465024967a12684b42c59bc58bc5
sirpanda25 <daniel.staats77@gmail.com>
2024-03-17 20:16:43 -0700

    Update farming.lua

--- farming.lua
+++ farming.lua
@@ -1,1 +1,22 @@
-turtle.back()
+isright = true
+for i = 1,8,-1 do
+    for i = 1,20,-1 do
+        turtle.forward()
+        block = turtle.inspectDown()
+        if 7 == block.state.age then
+            turtle.digDown()
+            turtle.suckDown()
+        end
+
+    end
+    if isright then
+        turtle.turnRight()
+        turtle.forward()
+        turtle.turnRight()
+    else
+        turtle.turnLeft()
+        turtle.forward()
+        turtle.turnLeft()
+    end
+
+end
